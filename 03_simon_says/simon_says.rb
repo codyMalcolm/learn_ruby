111a8ff824f8c83c45ca4c56d8be17b8ed5bff12
codyMalcolm <cody@codymalcolm.com>
2018-08-17 13:49:29 -0400

    Solve 03_simon_says

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,31 @@
 #write your code here
+def echo s
+  return s
+end
+
+def shout s
+  return s.upcase
+end
+
+def repeat s, n=2
+  a = []
+  n.times {
+    a.push(s)
+  }
+  return a.join(' ')
+end
+
+def start_of_word s, x
+  return s[0, x]
+end
+
+def first_word s
+  return s[/\w+/]
+end
+
+def titleize s
+  little_words = ['and', 'over', 'the']
+  a = s.split(' ')
+  a[0].upcase!
+  return a.map {|w| little_words.include?(w) ? w : w.capitalize}.join(' ')
+end
