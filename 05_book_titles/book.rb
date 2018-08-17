49569935716cd56bc2b726409b1eb8a3ae1bdd20
codyMalcolm <cody@codymalcolm.com>
2018-08-17 14:59:32 -0400

    Solve 05_book_titles/

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,10 @@
 class Book
-# write your code here
+  attr_reader :title
+
+  def title=s
+    @title = s.capitalize.split(' ').map {|w|
+      lower_case = ['and', 'in', 'the', 'of', 'a', 'an']
+      lower_case.include?(w) ? w : w.capitalize
+    }.join(' ')
+  end
 end
