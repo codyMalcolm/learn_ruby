23b0852e31e42d6224502a5ddf8007d791c9c13f
codyMalcolm <cody@codymalcolm.com>
2018-08-17 13:25:20 -0400

    Solve 02_calculator/

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,25 @@
 #write your code here
+def add x, y
+  return x + y
+end
+
+def subtract x, y
+  return x - y
+end
+
+def sum a
+  return a.reduce(0) {|a, b| a+=b}
+end
+
+def multiply a
+  return a.reduce(1) {|a, b| a*=b}
+end
+
+def power x, y
+  return x**y
+end
+
+def factorial x
+  return 1 if x < 2
+  return x*factorial(x-1)
+end
